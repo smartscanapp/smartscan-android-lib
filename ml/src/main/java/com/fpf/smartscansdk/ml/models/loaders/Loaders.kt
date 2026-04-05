8140dd26a7968b29e5dbc17e0cb47080962d78d5
d41dev <d41dev@proton.me>
2026-04-05 12:22:09 +0100

    refactor: pass file directly for file onnx loader

--- ml/src/main/java/com/fpf/smartscansdk/ml/models/loaders/Loaders.kt
+++ ml/src/main/java/com/fpf/smartscansdk/ml/models/loaders/Loaders.kt
@@ -4,8 +4,8 @@ import android.content.res.Resources
 import androidx.annotation.RawRes
 import java.io.File
 
-class FileOnnxLoader(private val path: String) : IModelLoader<ByteArray> {
-    override suspend fun load(): ByteArray = File(path).readBytes()
+class FileOnnxLoader(private val file: File) : IModelLoader<ByteArray> {
+    override suspend fun load(): ByteArray = file.readBytes()
 }
 
 class ResourceOnnxLoader(private val resources: Resources, @RawRes private val resId: Int) :
