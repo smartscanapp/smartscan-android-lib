2d8df089e78f6772ec6080641fec0081d10909d4
d41dev <d41dev@proton.me>
2026-06-28 09:15:32 +0100

    refactor: update fewShotClassification to use clusters

--- core/src/main/java/com/fpf/smartscansdk/core/classification/FewShotClassification.kt
+++ core/src/main/java/com/fpf/smartscansdk/core/classification/FewShotClassification.kt
@@ -1,21 +1,32 @@
 package com.fpf.smartscansdk.core.classification
 
-import com.fpf.smartscansdk.core.embeddings.StoredEmbedding
+import com.fpf.smartscansdk.core.cluster.Cluster
+import com.fpf.smartscansdk.core.cluster.ClusterId
+import com.fpf.smartscansdk.core.embeddings.Embedding
 import com.fpf.smartscansdk.core.embeddings.dot
 
 
-fun fewShotClassify(embedding: FloatArray, classPrototypes: List<StoredEmbedding>, classPrototypeCohesionScores: Map<Long, Float>): ClassificationResult{
+fun fewShotClassify(embedding: Embedding, classPrototypes: List<Cluster>): ClassificationResult{
     var bestSim = -1f
-    var bestPrototype: StoredEmbedding? = null
+    var bestPrototype: ClusterId? = null
 
     for(classPrototype in classPrototypes) {
-        val sim = classPrototype.embedding dot embedding
-        val cohesionScore = classPrototypeCohesionScores[classPrototype.id]
-        if(cohesionScore != null && sim >= cohesionScore && sim > bestSim){
+        val sim = when(embedding){
+            is Embedding.F32 -> {
+                require(classPrototype.embedding is Embedding.F32){"Embedding mismatch: expected F32"}
+                (classPrototype.embedding as Embedding.F32).vector dot embedding.vector
+            }
+            is Embedding.QInt8 -> {
+                require(classPrototype.embedding is Embedding.QInt8){"Embedding mismatch: expected QInt8"}
+                (classPrototype.embedding as Embedding.QInt8).vector dot embedding.vector
+            }
+        }
+        val threshold = classPrototype.metadata.meanSimilarity - classPrototype.metadata.stdSimilarity
+        if(sim >= threshold && sim > bestSim){
             bestSim = sim
-            bestPrototype = classPrototype
+            bestPrototype = classPrototype.prototypeId
         }
     }
-    val classId = bestPrototype?.id
+    val classId = bestPrototype
     return ClassificationResult(classId=classId, similarity = bestSim)
 }
