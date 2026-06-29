0b9c97ddbd01d1cf0ca6d32ef4a7c71b59b637d5
d41dev <d41dev@proton.me>
2026-06-29 16:52:08 +0100

    refactor: replace require statement with InvalidEmbeddingType error

--- core/src/main/java/com/fpf/smartscansdk/core/classification/FewShotClassification.kt
+++ core/src/main/java/com/fpf/smartscansdk/core/classification/FewShotClassification.kt
@@ -1,9 +1,12 @@
 package com.fpf.smartscansdk.core.classification
 
+import com.fpf.smartscansdk.core.SmartScanException
 import com.fpf.smartscansdk.core.cluster.Cluster
 import com.fpf.smartscansdk.core.cluster.ClusterId
 import com.fpf.smartscansdk.core.embeddings.Embedding
 import com.fpf.smartscansdk.core.embeddings.dot
+import com.fpf.smartscansdk.core.embeddings.toF32Embed
+import com.fpf.smartscansdk.core.embeddings.toQInt8Embed
 
 
 fun fewShotClassify(embedding: Embedding, classPrototypes: List<Cluster>): ClassificationResult{
@@ -13,12 +16,12 @@ fun fewShotClassify(embedding: Embedding, classPrototypes: List<Cluster>): Class
     for(classPrototype in classPrototypes) {
         val sim = when(embedding){
             is Embedding.F32 -> {
-                require(classPrototype.embedding is Embedding.F32){"Embedding mismatch: expected F32"}
-                (classPrototype.embedding as Embedding.F32).vector dot embedding.vector
+                if(classPrototype.embedding !is Embedding.F32) throw SmartScanException.InvalidEmbeddingType("Embedding mismatch: expected F32")
+                classPrototype.embedding.toF32Embed().vector dot embedding.vector
             }
             is Embedding.QInt8 -> {
-                require(classPrototype.embedding is Embedding.QInt8){"Embedding mismatch: expected QInt8"}
-                (classPrototype.embedding as Embedding.QInt8).vector dot embedding.vector
+                if(classPrototype.embedding !is Embedding.QInt8) throw SmartScanException.InvalidEmbeddingType("Embedding mismatch: expected QInt8")
+                classPrototype.embedding.toQInt8Embed().vector dot embedding.vector
             }
         }
         val threshold = classPrototype.metadata.meanSimilarity - classPrototype.metadata.stdSimilarity
